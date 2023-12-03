df03561b2130943433c96c6f68f2d7e2d63b13ca
CyrusNchege <cyruschegecc@gmail.com>
2023-12-04 00:43:16 +0300

    Add api using express

---
+++ server.js
@@ -0,0 +1,10 @@
+import routes from './routes';
+
+const express = require('express');
+
+const app = express();
+const port = 5000;
+
+app.use('/', routes);
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
